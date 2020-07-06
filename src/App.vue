e98ac232848495928def843190d5a86b43da527b
jcgsr <cabralguerra@gmail.com>
2020-07-06 08:05:25 -0300

    localStorage problem

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-		<NewTask @taskAdded="addTask" />
-		<TaskGrid :tasks="tasks" />
-	</div>
+  <div id="app">
+    <h1>Lista de Tarefas Vue</h1>
+    <TaskProgress :progress="progress" />
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChanged="toggleTaskState" />
+  </div>
 </template>
 
 <script>
 import TaskGrid from './components/TaskGrid.vue'
+import TaskProgress from './components/TaskProgress.vue'
 import NewTask from './components/NewTask.vue'
 
 export default {
-	components: { TaskGrid, NewTask },
+	components: { TaskGrid, NewTask, TaskProgress },
 	data() {
 		return {
-			tasks: [
-				{name: 'Estudar', pending: false}, 
-				{name: 'Programar', pending: true}, 
-			]
+			tasks: []
+		}
+	},
+	computed: {
+		progress() {
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		}
+	},
+	watch: {
+		tasks: {
+			deep: true,
+			handler() {				
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
 		}
 	},
 	methods: {
@@ -30,30 +44,45 @@ export default {
 					pending: task.pending || true
 				})
 			}
+		},
+		deleteTask(i) {
+			this.tasks.splice(i, 1)
+		},
+		toggleTaskState(i) {
+			this.tasks[i].pending = !this.tasks[i].pending
+		},
+		created() {
+			const json = localStorage.getItem('tasks')
+			const array = JSON.parse(json)
+			this.tasks = Array.isArray(array) ? array : []
 		}
 	}
 }
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+h1 {
+  padding: 2rem;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
