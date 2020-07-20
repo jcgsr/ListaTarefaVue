01084bbec1863186dc8565ffbd2ca2422b028b20
jcgsr <cabralguerra@gmail.com>
2020-07-20 19:51:28 -0300

    emoji

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -6,7 +6,7 @@
                 @taskStateChanged="$emit('taskStateChanged', i)"
                 :task="task"></Task>
         </template>
-        <p v-else class="no-task">Sua vida está em dia :)</p>
+        <p v-else class="no-task">Sua vida está em dia 😄</p>
     </div>
 </template>
 
