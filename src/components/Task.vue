518128b6ea616903084ff2383713e8d3aa33b668
jcgsr <cabralguerra@gmail.com>
2020-07-20 19:41:06 -0300

    task bug fix

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,7 @@
 <template>
-  <transition    
-    enter-active-class="animate_animated animate__zoomIn"
-    leave-active-class="animate_animated animate__zoomOut"
-  >
+  <transition enter-active-class="animate_animated animate__zoomIn"
+  leave-active-class="animate_animated animate__zoomOut"
+  move>
     <fieldset
       @click="$emit('taskStateChanged', task)"
       class="task animate__animated animate__zoomIn"
@@ -15,45 +14,84 @@
 </template>
 
 <script>
-import Task from "./Task.vue";
 export default {
-  components: { Task },
   props: {
-    tasks: { type: Array, required: true }
+    task: { type: Object, required: true }
+  },
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending
+      };
+    }
   }
 };
 </script>
 
 <style>
-p {
-  color: #0a0455;
+fieldset {
+  display: flex;
+  border: none;
+  padding: 0;
+  margin: 0;
 }
-.task-grid span {
+
+fieldset p {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  color: beige;
+}
+
+.task {
+  position: relative;
+  box-sizing: border-box;
+  width: 20rem;
+  height: 5rem;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pending {
+  border-left: 12px solid rgb(7, 7, 150);
+  background-color: #0a0455;
+}
+
+.done {
+  color: #ddd;
+  border-left: 12px solid rgb(59, 55, 55);
+  background-color: rgb(12, 1, 1);
+  text-decoration: line-through;
+}
+
+.pending .close {
+  background-color: rgb(7, 7, 150);
+  color: beige;
+}
+
+.done .close {
+  background-color: rgb(59, 55, 55);
+}
+
+.close {
+  position: absolute;
+  right: 2px;
+  top: 2px;
+  font-size: 0.9rem;
+  font-weight: 600;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
   display: flex;
   justify-content: center;
 }
-
-.task-grid .task {
-  margin: 10px;
-}
-
-.no-task {
-  color: #aaa;
-  font-size: 1.7rem;
-}
-.popup-move {
-  transition: all 300ms;
-}
-.popup-enter,
-.popup-leave-to {
-  transform: scale(0);
-}
-.popup-enter-active {
-  transition: 400ms;
-}
-.popup-leave-active {
-  transition: 400ms;
-  position: absolute;
-}
-
 </style>
