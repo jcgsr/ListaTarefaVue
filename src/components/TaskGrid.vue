5430e21355c11b461db25c1ad8cdcd84e1790217
jovane rocha <55019499+jcgsr@users.noreply.github.com>
2020-07-20 16:46:40 -0300

    Revert "Update TaskGrid.vue"

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,36 +1,34 @@
 <template id="grid">
   <div class="task-grid">
-    <transition-group name="popup">
-      <Task
-        v-for="(task, i) in tasks"
-        :key="task.name"
-        @taskStateChanged="$emit('taskStateChanged', i)"
-        @taskDeleted="$emit('taskDeleted', i)"
-        :task="task"
-      ></Task>
-    </transition-group>
-
-    <p v-if="tasks.length < 1" class="no-task">Sua vida está em dia 😃</p>
+    <template v-if="tasks.length ">
+      <Task v-for="(task, i) in tasks" :key="task.name" 
+      @taskStateChanged="$emit('taskStateChanged', i)"
+      @taskDeleted="$emit('taskDeleted', i)" :task="task"></Task>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia 😃</p>
   </div>
 </template>
 
 <script>
-import Task from "./Task.vue";
+import Task from './Task.vue' 
 export default {
   components: { Task },
   props: {
-    tasks: { type: Array, required: true }
+    tasks: { type: Array, required: true}
   }
-};
+}
+
 </script>
 
 <style>
-p {
-  color: #0a0455;
-}
-.task-grid span {
+.task-grid {
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
+}
+
+p {
+  color:  #0a0455;
 }
 
 .task-grid .task {
@@ -40,19 +38,5 @@ p {
 .no-task {
   color: #aaa;
   font-size: 1.7rem;
-}
-.popup-move {
-  transition: all 300ms;
-}
-.popup-enter,
-.popup-leave-to {
-  transform: scale(0);
-}
-.popup-enter-active {
-  transition: 400ms;
-}
-.popup-leave-active {
-  transition: 400ms;
-  position: absolute;
 }
 </style>
